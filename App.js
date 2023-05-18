3cfaca917f5542cfe4856512a02c8fcbf0e6a343
Harinireddy05 <86099787+Harinireddy05@users.noreply.github.com>
2023-05-18 19:08:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+/*import EmployeeList from './EmployeeList'*/
+function App(){
+  return (
+    <div className="App">
+    {/*<EmployeeList/>*/}
+    </div>
+  );
+}
+
+export default App;
